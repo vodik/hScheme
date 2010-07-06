7bf6b12ed90b717b032ea90a3eedf7e43dd9b141
Simon Gomizelj <simongmzlj@gmail.com>
2010-07-06 16:33:51 -0400

    adding util module

---
+++ Util.hs
@@ -0,0 +1,10 @@
+module Util where
+
+import Numeric
+
+oct2dig x = fst $ readOct x !! 0
+hex2dig x = fst $ readHex x !! 0
+bin2dig  = bin2dig' 0
+bin2dig' digint "" = digint
+bin2dig' digint (x:xs) = let old = 2 * digint + (if x == '0' then 0 else 1)
+                         in bin2dig' old xs
